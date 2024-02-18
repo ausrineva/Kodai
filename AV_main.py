101eef8004e37628d885b09fa994b12d204506b8
ausrineva <159789891+ausrineva@users.noreply.github.com>
2024-02-18 23:34:53 +0200

    Update AV_main.py
    Prideta saknies funkcija, optimizuotas meniu blokas

--- AV_main.py
+++ AV_main.py
@@ -13,6 +13,10 @@ def sudauginti(x, y):
     return x*y
 
 
+def saknis(skaicius):
+    return math.sqrt(skaicius)
+
+
 def padalinti(x, y):
     return x/y
 
@@ -24,19 +28,23 @@ def meniu():
     print("2. Atimti")
     print("3. Sudauginti")
     print("4. Dalinti")
+    print("5. Traukti sakni")
 
 
+meniu()
 while True:
 
     pasirinkimas = input("Kurią operaciją norite atlikti?")
 
     if pasirinkimas in ('1', '2', '3', '4'):
         try:
-            x = int(input("Koks jūsų x?"))
-            y = int(input("Koks jūsų y?"))
+            x = int(input("Koks jūsų x?   "))
+            y = int(input("Koks jūsų y?   "))
         except ValueError:
             print("Klaida. Įrašykite skaičių")
             continue
+    elif pasirinkimas == '5':
+        skaicius = int(input("Koks jusu skaicius?  "))
 
     if pasirinkimas == '1':
         print(x, "+", y, "=", sudeti(x, y))
@@ -46,6 +54,8 @@ while True:
         print(x, "*", y, "=", sudauginti(x, y))
     elif pasirinkimas == '4':
         print(x, "/", y, "=", padalinti(x, y))
+    elif pasirinkimas == '5':
+        print("Saknis is skaiciaus =", saknis(skaicius))
 
         kitas = input("Ar norite atlikti kitus veiksmus? (taip/ne):")
         if kitas == "ne":
