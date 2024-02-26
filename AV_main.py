1f6efbf6901308d2395e87b794b1ca44fd2849e9
ausrineva <159789891+ausrineva@users.noreply.github.com>
2024-02-26 15:49:36 +0200

    Update AV_main.py
    Programa su dėstytojo pavyzdiniu kodu

--- AV_main.py
+++ AV_main.py
@@ -8,7 +8,71 @@ app = Flask(__name__)
 def sveikivisi():
     return "<p>Skaičiuotuvas<p>"
 
+    from flask import Flask, request
 
+
+app = Flask(__name__)
+
+
+skaicius = 0  # apsirasome kintamaji ( Globalus )
+
+
+def sudetis(pirmas, antras):
+    return pirmas+antras
+
+
+@app.route("/")  # Route 1
+def hello_world():
+
+    return f"""
+                <form action="/skaicius">
+                    <label for="test">skaicius 1</label><br>
+                        <input type="text" id="test" name="test" value="0"><br>
+                        </br></br>
+
+                    <label for="test2">skaicius 2</label><br>
+                        <input type="text" id="test2" name="test2" value="0"><br><br>
+                        </br></br>
+                        
+                    <label for="[[__ID__]]">skaicius 2</label><br>   
+                        <input type="text" id="[[__ID__]]" name="[[__ID__]]" value="0"><br><br>
+                        </br></br>
+
+                    <input type="submit" value="Submit">
+                </form> 
+            """
+
+
+@app.route("/labas")  # Route 2
+def sakyk_labas():
+    global skaicius  # Naudoju globalu kintamaji
+    skaicius = skaicius + 1  # kaskart atidare pridedam 1
+    return f"Labas {skaicius}"
+
+    '''
+        /skaicius?test=100
+        /skaicius?test=0  &  test2=0
+    '''
+
+
+@app.route("/skaicius")  # Route 3
+def skaiciavimo():
+    # UZKLAUSA. ARGUMENTAI. METODAS()
+    # Pasiimam argumenta is URL pvz.: /skaicius?test=100
+    skaicius = request.args.get("test")
+    # Pasiimam argumenta 2 is URL pvz.: /skaicius?test2=100
+    skaicius2 = request.args.get("test2")
+
+    suma = sudetis(int(skaicius2), int(skaicius))
+
+    return f"Tavo ivestas skaicius: {suma}"
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+'''
 if __name__ == "__main__":
     app.run()
 
@@ -75,3 +139,4 @@ while True:
             break
         else:
             print("Suveskite iš naujo taip arba ne teisingai")
+'''
